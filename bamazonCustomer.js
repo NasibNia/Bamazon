d649da9dac2ee7b3c0341dd1799f681533a39d16
NasibNia <nasibnia@gmail.com>
2018-09-04 15:04:38 -0700

    find product runs before validate

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -54,127 +54,10 @@ function manageProduct () {
         }
     ]).then(function(response){ 
         
-        validate(response.userItem , manageProduct , takeOrder);
-        // console.log("checkId before" +checkId);
-
-        // idAvailable(parseInt(response.userItem));
-        // console.log("checkId after" +checkId);
-        // if the input is either q or Q   
-        // if ((response.userItem).toLowerCase() === "q"){
-        //     exit();
-        // } //check if the id is a number 
-        // else if (!isNaN(response.userItem)){
-        //     // console.log(" the ID you entered is  a number");
-
-        //     //check if the number user entered exists in the list of item ids
-            
-        //     console.log(idAvailable(parseInt(response.userItem)));
-        //     if (!idAvailable(parseInt(response.userItem), getIds)) {
-        //         console.log("the id you entered is not available please try again");
-        //         manageProduct();
-        //     } // if id is a valide number; here is where all the action happens: 
-        //     else {
-        //         console.log("good, the id "+ response.userItem+" is valid");
-        //         // takeOrder(parseInt(response.userItem));
-        //     }
-
-        // } // if the input is any other than Q or a number  
-        // else {
-        //     console.log(" this input is not acceptable");
-        //     manageProduct();
-
-        // } 
+        validate(response.userItem , "item_id", manageProduct , takeOrder); 
 
     });
 } 
-
-function validate(input, callback1 , callback2){
-    // if the input is either q or Q   
-    if ((input).toLowerCase() === "q"){
-        exit();
-    }//check if the id is a number 
-    else if (!isNaN(input)){
-        console.log(" the ID you entered is  a number");
-
-        //check if the number user entered exists in the list of item ids  
-            // var checkId = false;
-            connection.query("SELECT item_id FROM products", function(err, data){
-                if (err) throw err;
-                var ids = [];
-                for(var i = 0; i < data.length ; i++){
-                    ids.push(parseInt(data[i].item_id));
-                }
-                // console.log(ids);
-                // console.log(id);
-                if(ids.includes(parseInt(input))) {
-                    console.log("inside true");
-                    // checkId = true;
-                    callback2();        
-                } else{
-                    console.log("inside false");
-                    // checkId = false;
-                    callback1();
-                } 
-            });
-            
-        // } // if id is a valide number; here is where all the action happens: 
-        // else {
-        //     callback2(input);
-        //     console.log("yayyyyy, inside callback2");
-        // }
-
-    } // if the input is any other than Q or a number  
-    else {
-        console.log(" this input is not acceptable");
-        callback1();
-
-    } 
-}
-
-// function idAvailable(id, callback){
-//     var ids = callback();
-//     if(ids.includes(id)) {
-//         console.log("inside true");
-//         return true;
-//     } 
-//     return false;
-// } 
-
-// function getIds(){
-//     var arr = [];
-//     connection.query("SELECT item_id FROM products", function(err, data){
-//         if (err) throw err;
-//         for(var i = 0; i < data.length ; i++){
-//             arr.push(parseInt(data[i].item_id));
-//         }
-        
-//     });
-//     return arr;
-// }
-
-
-// function idAvailable(id){
-//     var checkId = false;
-//     connection.query("SELECT item_id FROM products", function(err, data){
-//         if (err) throw err;
-//         var ids = [];
-//         for(var i = 0; i < data.length ; i++){
-//             ids.push(parseInt(data[i].item_id));
-//         }
-//         // console.log(ids);
-//         // console.log(id);
-//         if(ids.includes(id)) {
-//             console.log("inside true");
-//             checkId = true;
-  
-//         } else{
-//             console.log("inside false");
-//             checkId = false;
-
-//         } 
-
-//     });
-// }
 
 function takeOrder (id){
     inquirer.prompt([
@@ -184,9 +67,106 @@ function takeOrder (id){
             name : "quantity"
         }
     ]).then(function(response){
-        validate(response.quantity , takeOrder ,takeOrder);
+        validate(response.quantity , "stock_quantity" , takeOrder ,findProduct(id,parseInt(response.quantity)));
+
     });
 }
+
+function findProduct (id , buyerOrder) {
+    var queryString = "SELECT * FROM products WHERE ? ";
+    connection.query(queryString,
+        {
+            item_id : id
+        } , function(err, data){
+        if (err) throw err;
+        
+        var onStockNumbers = parseInt(data.stock_quantity);
+        
+        if (buyerOrder <= onStockNumbers) {
+            console.log("congrats, you are about to buy " + buyerOrder + "of item id " + id + "( " + data.product_name + " )\n");
+            var updatedNumbers = onStockNumbers - buyerOrder; 
+            updateProducts(id , updatedNumbers);
+        } else {
+            console.log ("sorry we do not have the number you asked in the stock, please enter a valid number");
+            takeOrder(id);
+        }
+    });
+}
+
+function updateProducts(item , numbers) {
+    var queryString = "UPDATE products SET ? WHERE ?";
+    connection.query(queryString,[
+        {
+             stock_quantity : numbers
+        },
+        {
+            item_id : item
+        }] , function(err, data){
+        if (err) throw err;
+        console.log("great! your purchase completed, Here is an updated list of products:");
+        //Let's have a look at our updated table (pulling straight from our database!!!!)
+        review() ;
+    });
+
+}
+
+
+function validate(input, category , callback1 , callback2){
+    var options = [];
+    var variable;
+    if(category === "item_id"){
+        variable = "item_id";
+    } else if (category === "stock_quantity"){
+        variable = "stock_quantity";
+    }
+    
+    // if the input is either q or Q   
+    if ((input).toLowerCase() === "q"){
+        exit();
+    }//check if the id is a number 
+    else if (!isNaN(input)){
+        // console.log(" the ID you entered is  a number");
+        //check if the number user entered exists in the list of item options  
+            var querySrt = "SELECT " + variable + " FROM products" ;
+            connection.query(querySrt , function(err, data){
+                if (err) throw err;
+                console.log (querySrt);
+                // console.log(data);
+                for(var i = 0; i < data.length ; i++){
+                    options.push(parseInt(data[i][variable]));
+                }
+                // console.log(options);
+                // console.log(input);
+                if(category === "item_id"){
+                    if(options.includes(parseInt(input))) {
+                        console.log("inside true");
+                        callback2();        
+                    } else{
+                        console.log("inside false");                    
+                        callback1();
+                    } 
+                } else {
+                    if(parseInt(input) > 0 ) {
+                        console.log("this value is more than what we have in stock ");
+                        callback2();
+                    } else{
+                        console.log("great! we have this amount in our stock");
+                        callback1();
+                    }
+                }
+                
+            });    
+
+    } // if the input is any other than Q or a number  
+    else {
+        console.log(" this input is not acceptable");
+        callback1();
+
+    } 
+}
+
+
+
 
 
 // this function ends the connection to database
